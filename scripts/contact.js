330241d3684f554837d5ab279bc6d39e804db127
arianacic <ArianaCharlize.IzonCharles@edu.sait.ca>
2026-04-14 22:59:37 -0600

    contact page js

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,3 +2,22 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+// get the main contact page container
+const contactPage = document.getElementById("contact-page");
+
+// get the submit button
+const submitButton = document.getElementById("submit-button");
+
+// when the button is clicked:
+submitButton.addEventListener("click", function(event) {
+
+    // prevent the form from refreshing the page
+    event.preventDefault();
+
+    // replace the entire main content with a thank-you message
+    contactPage.innerHTML = "<p id='thank-you'>Thank you for your message</p>";
+
+    // style the message to be 24px
+    const thankYou = document.getElementById("thank-you");
+    thankYou.style.fontSize = "24px";
+});
